63b00d07f92b852d99444b7c6fdc3bf8436e561e
sergey sergeev <dir@itwebnet.ru>
2022-05-16 10:47:27 +0300

    1

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -40,6 +40,15 @@ export class NavMenu extends Component {
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
                 </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                </NavItem>
                 <LoginMenu>
                 </LoginMenu>
               </ul>
---
+++ ClientApp/src/components/bottom.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export class Bottom extends Component{
+
+
+    render(){
+        return(
+        <div className='bottom'>bottom</div>
+        )
+    }
+}
